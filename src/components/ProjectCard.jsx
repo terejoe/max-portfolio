5ffaf132368fa1e2d8346bfcabae4417de6602b9
terejoe <nzeamalunkechinyere@gmail.com>
2024-08-05 18:33:40 +0100

    added a cursor pointer to my card project

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -16,12 +16,12 @@ export default function ProjectCard({title, text, href,lang}) {
 
   return (
     <>
-      <div onClick={openModal} className='border-[#97A1AB] border-2 justify-center items-center w-[350px] shrink-0'>
+      <div onClick={openModal} className='border-[#97A1AB] border-2 justify-center items-center w-[350px] shrink-0 cursor-pointer'>
         <div className='p-6'>
           <LuFolderDot size={25} style={{color:"#E3E5E8" }} className='mb-2'/>
           <p className='monument-extended text-xl text-white mb-1'>{title}</p>
-          <p className='leading-6 text-sm text-[#ADB5BD]'> {text} </p>              
-        </div>
+          <p className='leading-6 text-sm text-[#ADB5BD] line-clamp-4'> {text} </p>              
+        </div>i
 
         <button className='text-[#1469FF] px-6 py-3 flex items-center mx-4 border-none uppercase text-sm mb-2'><a href={href}>View Project<GoArrowUpRight className='ml-4 inline' size={18}/></a></button>
 
