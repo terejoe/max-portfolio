228874f051816d797a34f6e32ebeb7b008e735da
terejoe <nzeamalunkechinyere@gmail.com>
2024-08-15 15:33:27 +0100

    integrated lazy load and suspense

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,27 @@
-import { Route, Routes} from "react-router-dom";
-import LandingPage from "./pages/LandingPage";
-import BlogPost from "./pages/BlogPost";
+import React, { Suspense, lazy } from "react";
+import { Route, Routes } from "react-router-dom";
+// import LandingPage from "./pages/LandingPage";
+// import BlogPost from "./pages/BlogPost";
 import ScrollToTop from "./hooks/ScrollToTop";
 
+
+const LandingPage = React.lazy(() => import("./pages/LandingPage")); 
+const BlogPost = React.lazy(() => import("./pages/BlogPost"));
+
 function App() {
-  
+
   return (
-    
     <>
       <ScrollToTop />
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/blog-post/:blogId" element={<BlogPost />} />
-      </Routes>
+      <Suspense fallback={<div>Loading..</div>}>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/blog-post/:blogId" element={<BlogPost />} />
+        </Routes>
+      </Suspense>
+
     </>
-    
-  )
+  );
 }
 
-export default App
+export default App;
